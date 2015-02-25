410a55ff1c8f58ef3eabb03b76732f20db587a07
Maltuin <maxime.margaine@yahoo.fr>
2015-02-25 15:05:22 +0100

    Prompt pour la class"

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import org.omg.CORBA.DefinitionKind;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,16 +9,44 @@ import java.io.InputStreamReader;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Enter your name: ");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String name = null;
-        try {
-            name = br.readLine();
-        } catch (IOException ioe) {
-            System.out.println("IO error trying to read your name!");
-            System.exit(1);
+        System.out.print("Choose your class : " + "\n" +
+                "Power : 1 = Jedi, 2 = Sith" + "\n" +
+                "No Power 3 = bountyHunter, 4 = ImperialAgent, 5 = Mercenary, 6 = Jawa" + "\n");
+        int Define = 0;
+        while (Define == 0) {
+            String read = ReadPrompt();
+            int HeroClass = Integer.parseInt(read);
+            Define = 1;
+            switch (HeroClass) {
+                case 1:
+                    Jedi hero = new Jedi("name");
+                    System.out.println(hero.name);
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    Define = 0;
+                    System.out.println("This class doesn't exist");
+            }
         }
-        Power hero = new Power(name, 0);
-        System.out.println(hero.name);
+
+    }
+
+    public static String ReadPrompt() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            return br.readLine();
+        } catch (IOException ioe) {
+            return "Erreur";
+        }
     }
 }
