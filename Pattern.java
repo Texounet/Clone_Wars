00ed552da42ef7354b10a8aa0c599f78a911a7d4
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2015-02-25 10:19:12 +0100

    Ajout des class ennemi.java et room.java + Getter setter Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -8,4 +8,46 @@ public class Pattern {
     protected int force;
     protected int intelligence;
     protected int lvl;
+    
+    public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getHP(){
+		return this.hp;
+	}
+	
+	public void setDef(int def) {
+		this.def = def;
+	}
+	public int getDef(){
+		return this.def;
+	}
+	
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public int getPower(){
+		return this.power;
+	}
+	
+	public void setForce(int force) {
+		this.force = force;
+	}
+	public int getForce(){
+		return this.force;
+	}
+	
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+	public int getIntelligence(){
+		return this.intelligence;
+	}
+	
+	public void setLvl(int lvl) {
+		this.lvl = lvl;
+	}
+	public int getLvl(){
+		return this.lvl;
+	}
 }
---
+++ ennemi.java
@@ -0,0 +1,6 @@
+/**
+ * Created by tex on 25/02/2015.
+ */
+public class ennemi {
+
+}
