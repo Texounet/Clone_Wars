fc12685c1d00c3cfa2b03734938a5954d5f22765
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2015-02-25 16:24:15 +0100

    re

---
+++ Clone.java
@@ -0,0 +1,13 @@
+
+public class Clone extends Ennemis{
+	
+	public Clone(int level){
+		hp = 100;
+		def = 100;
+		power = 100;
+		force = 0;
+		intelligence = 0;
+		lvl = 1;
+		//System.out.println("Clone");
+	}
+}
