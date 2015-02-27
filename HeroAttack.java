8344647007d60ed47730787a9ae23f2f81f6786a
Maltuin <maxime.margaine@yahoo.fr>
2015-02-27 17:21:48 +0100

    Ajout des degats

--- HeroAttack.java
+++ HeroAttack.java
@@ -5,23 +5,34 @@ public abstract class HeroAttack {
     abstract public void launch(Hero hero, Hero enemy);
 
     static public class physical_attack extends HeroAttack{
-        public void launch(Hero hero, Hero enemy) {
-            System.out.println("BWAAAAAAAAAAAH");
+        public void launch(Hero fighter, Hero defender) {
+            int degat = fighter.stats.power - defender.stats.def;
+            System.out.println(fighter.stats.name + " inflige  "+degat+" a "+defender.stats.name);
+            defender.stats.hp -= degat;
         }
     }
 
     static class force_attack extends HeroAttack{
-        public void launch(Hero hero, Hero enemy) {
+        public void launch(Hero fighter, Hero defender) {
+            int degat = (fighter.stats.force/10)*2;
+            System.out.println(fighter.stats.name + " inflige  "+degat+" a "+defender.stats.name);
+            defender.stats.hp -= degat;
         }
     }
 
     static public class armed_attack extends HeroAttack{
-        public void launch(Hero hero, Hero enemy) {
+        public void launch(Hero fighter, Hero defender) {
+            int degat = fighter.stats.intelligence - defender.stats.def;
+            System.out.println(fighter.stats.name + " inflige  "+degat+" a "+defender.stats.name);
+            defender.stats.hp -= degat;
         }
     }
 
     static public class batarang extends HeroAttack{
-        public void launch(Hero hero, Hero enemy) {
+        public void launch(Hero fighter, Hero defender) {
+            int degat = fighter.stats.power - defender.stats.def;
+            System.out.println(fighter.stats.name + " inflige  "+degat+" a "+defender.stats.name);
+            defender.stats.hp -= degat;
         }
     }
 }
