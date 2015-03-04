a6cda3df5fb2620cca89ce73ce90020bda26bc03
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2015-03-04 17:23:10 +0100

    finition

--- Item.java
+++ Item.java
@@ -4,10 +4,11 @@ public abstract class Item {
 	 
 	 static public class popo extends Item {
         public void item(Character hero) {
-        	if(hero.stats.gold >= 20){
+        	if(hero.stats.gold >= 50){
         		System.out.println("Vous venez d'acheter un potion.");
         		int tempo = hero.stats.nbPotion + 1;
         		hero.stats.nbPotion = tempo;
+        		hero.stats.setGold(hero.stats.getGold() - 50);
         	}
         	else{
         		System.out.println("Vous avenez pas assez d'or.");
@@ -17,10 +18,11 @@ public abstract class Item {
 	 
 	 static public class att extends Item {
         public void item(Character hero) {
-        	if(hero.stats.gold >= 20){
+        	if(hero.stats.gold >= 50){
         		System.out.println("Vous venez d'augmenter voter attaque.");
         		int tempo = (hero.stats.getPower() / 100) * 10;
         		hero.stats.setPower(tempo);
+        		hero.stats.setGold(hero.stats.getGold() - 50);
         	}
         	else{
         		System.out.println("Vous avenez pas assez d'or.");
@@ -30,10 +32,11 @@ public abstract class Item {
 	 
 	 static public class def extends Item {
         public void item(Character hero) {
-        	if(hero.stats.gold >= 20){
+        	if(hero.stats.gold >= 50){
         		System.out.println("Vous venez d'augmenter votre defense.");
         		int tempo = (hero.stats.getDef() / 100) * 10;
     			hero.stats.setDef(tempo);
+    			hero.stats.setGold(hero.stats.getGold() - 50);
         	}
     		else{
     			System.out.println("Vous avenez pas assez d'or.");
