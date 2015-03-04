5fa6e25e9a728374d994f5e604dd9f8ddb1c9e75
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2015-03-04 13:30:28 +0100

    Mag + Item

---
+++ Item.java
@@ -0,0 +1,43 @@
+
+public abstract class Item {
+	 abstract public void item(Character hero);
+	 
+	 static public class popo extends Item {
+        public void item(Character hero) {
+        	
+        }
+	 }
+	 
+	 static public class att extends Item {
+        public void item(Character hero) {
+        	int tempo = (hero.stats.getPower() / 100) * 10;
+    		hero.stats.setPower(tempo);
+        }
+	 }
+	 
+	 static public class def extends Item {
+        public void item(Character hero) {
+        	int tempo = (hero.stats.getDef() / 100) * 10;
+    		hero.stats.setDef(tempo);
+        }
+	 }
+	 
+	 static public class bonjour extends Item {   
+        public void item(Character hero) {
+        	System.out.println("Vous: 'Bonjour'");
+    		System.out.println("Marchand: 'Bonjour vous m'avez l'air fort sympatique voici une potion offerte par la maison'.");
+    		System.out.println("Vous obtenez une potion");
+        }
+	 }
+	 
+	 static public class help extends Item {   
+	        public void item(Character hero) {
+	        	System.out.println("Liste des commandes:");
+	    		System.out.println("'exit': Quite le magasin");
+	    		System.out.println("'att': Améliore l'attaque");
+	    		System.out.println("'def': Améliore la defense");
+	    		System.out.println("'popo': Achete un potion");
+	    		System.out.println("'bonjour': Dire bonjour au vendeur");
+	        }
+		 }
+}
---
+++ Magasin.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Magasin {
+	
+	public  Magasin(){
+	}
+	
+	public void menu(){
+		int nb_ac = 0;
+		System.out.println("Bienvenut dans le magasin");
+		while(nb_ac == 0){
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String Action = null;
+			try {
+				Action = br.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IO error trying to read your name!");
+				System.exit(1);
+			}
+			if(Action.compareTo("exit") == 0){
+				nb_ac = 1;
+			}
+			else if(Action.compareTo("bonjour") == 0){
+				bonjour();
+			}
+			else if(Action.compareTo("h") == 0 || Action.compareTo("help") == 0 ){
+				help();
+			}
+			else{
+				System.out.println("Votre commande est invalide. Consulter l'aide grace à la commande h ou 'help'");
+			}
+		}
+	}
+}
