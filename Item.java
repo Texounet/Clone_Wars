89d1845b55cc9ab75cebf4fbb162c133bcb0f1a1
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2015-03-04 17:03:15 +0100

    magasin ok

--- Item.java
+++ Item.java
@@ -4,21 +4,40 @@ public abstract class Item {
 	 
 	 static public class popo extends Item {
         public void item(Character hero) {
-        	
+        	if(hero.stats.gold >= 20){
+        		System.out.println("Vous venez d'acheter un potion.");
+        		int tempo = hero.stats.nbPotion + 1;
+        		hero.stats.nbPotion = tempo;
+        	}
+        	else{
+        		System.out.println("Vous avenez pas assez d'or.");
+        	}
         }
 	 }
 	 
 	 static public class att extends Item {
         public void item(Character hero) {
-        	int tempo = (hero.stats.getPower() / 100) * 10;
-    		hero.stats.setPower(tempo);
+        	if(hero.stats.gold >= 20){
+        		System.out.println("Vous venez d'augmenter voter attaque.");
+        		int tempo = (hero.stats.getPower() / 100) * 10;
+        		hero.stats.setPower(tempo);
+        	}
+        	else{
+        		System.out.println("Vous avenez pas assez d'or.");
+        	}
         }
 	 }
 	 
 	 static public class def extends Item {
         public void item(Character hero) {
-        	int tempo = (hero.stats.getDef() / 100) * 10;
-    		hero.stats.setDef(tempo);
+        	if(hero.stats.gold >= 20){
+        		System.out.println("Vous venez d'augmenter votre defense.");
+        		int tempo = (hero.stats.getDef() / 100) * 10;
+    			hero.stats.setDef(tempo);
+        	}
+    		else{
+    			System.out.println("Vous avenez pas assez d'or.");
+    		}
         }
 	 }
 	 
@@ -31,13 +50,13 @@ public abstract class Item {
 	 }
 	 
 	 static public class help extends Item {   
-	        public void item(Character hero) {
-	        	System.out.println("Liste des commandes:");
-	    		System.out.println("'exit': Quite le magasin");
-	    		System.out.println("'att': Améliore l'attaque");
-	    		System.out.println("'def': Améliore la defense");
-	    		System.out.println("'popo': Achete un potion");
-	    		System.out.println("'bonjour': Dire bonjour au vendeur");
-	        }
-		 }
+	    public void item(Character hero) {
+	       	System.out.println("Liste des commandes:");
+	    	System.out.println("'exit': Quite le magasin");
+	    	System.out.println("'att': Améliore l'attaque");
+	    	System.out.println("'def': Améliore la defense");
+	    	System.out.println("'popo': Achete un potion");
+	    	System.out.println("'bonjour': Dire bonjour au vendeur");
+	     }
+	 }
 }
